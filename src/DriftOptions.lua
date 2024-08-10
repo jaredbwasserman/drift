aa0fc008862a80c4ecd1eaa3538c6940f3686d2d
Jared Wasserman <jaredbwasserman@gmail.com>
2024-08-10 00:06:00 -0700

    Fix InterfaceOptions_AddCategory

--- src/DriftOptions.lua
+++ src/DriftOptions.lua
@@ -57,6 +57,29 @@ local function createButton(name, point, relativeFrame, relativePoint, xOffset, 
 	return button
 end
 
+--Locally define a function removed from Blizzard's API in 11.0
+local InterfaceOptions_AddCategory = InterfaceOptions_AddCategory
+if not InterfaceOptions_AddCategory then
+	InterfaceOptions_AddCategory = function(frame, addOn, position)
+		-- cancel is no longer a default option. May add menu extension for this.
+		frame.OnCommit = frame.okay;
+		frame.OnDefault = frame.default;
+		frame.OnRefresh = frame.refresh;
+
+		if frame.parent then
+			local category = Settings.GetCategory(frame.parent);
+			local subcategory, layout = Settings.RegisterCanvasLayoutSubcategory(category, frame, frame.name, frame.name);
+			subcategory.ID = frame.name;
+			return subcategory, category;
+		else
+			local category, layout = Settings.RegisterCanvasLayoutCategory(frame, frame.name, frame.name);
+			category.ID = frame.name;
+			Settings.RegisterAddOnCategory(category);
+			return category;
+		end
+	end
+end
+
 -- Global functions
 function DriftHelpers:SetupConfig()
 	-- Initialize config options
